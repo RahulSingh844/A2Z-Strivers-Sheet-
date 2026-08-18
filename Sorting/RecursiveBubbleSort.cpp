6c35afba46a76aa5828145aeb871f9ca5de93367
RahulSingh844 <rahulchd30082005@gmail.com>
2026-08-19 02:20:50 +0530

    This is better approach of recursive BubbleSort

--- Sorting/RecursiveBubbleSort.cpp
+++ Sorting/RecursiveBubbleSort.cpp
@@ -16,7 +16,18 @@ void bubbleSort(vector <int> &arr , int n ,int i){
 }
 
 
-
+// Better approach TC = O(n^2)
+// space complexity = O(n)
+void bubbleSortBetter(vector <int> &arr , int n){
+    if(n==1) return;
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        bubbleSortBetter(arr , n-1);
+    
+}
 
 int main ( ){
     int n;

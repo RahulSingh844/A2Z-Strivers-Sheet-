a8c764c2f447b8f860b4db80042a400fb44c9733
RahulSingh844 <rahulchd30082005@gmail.com>
2026-09-17 00:12:23 +0530

    Set Mstrix zero optimal approach

--- MediumArrayQue/SetMatrixZero.cpp
+++ MediumArrayQue/SetMatrixZero.cpp
@@ -69,6 +69,69 @@ void set1(vector<vector<int>> &mat){
     }
 }
 
+//Better Approach
+void set3(vector<vector<int>> &mat){
+    int n=mat.size();
+    int m=mat[0].size();
+    vector<int> row(n,0);
+    vector<int> col(m,0);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==0){
+                row[i]=1;
+                col[j]=1;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(row[i] || col[j]){
+                mat[i][j]=0;
+            }
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+// optimal approach
+void OptimalApproach(vector<vector<int>> &mat){
+    int n=mat.size();
+    int m=mat[0].size();
+    int col0=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==0){
+                mat[i][0]=0;//row
+                if(j!=0){
+                    mat[0][j]=0;//col
+                }
+                else col0=0;
+            }
+        }
+    }
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(mat[i][j]!=0){
+                if(mat[i][0]==0 || mat[0][j]==0){
+                    mat[i][j]=0;
+                }
+            }
+        }
+    }
+    if(mat[0][0]==0){
+        for(int j=0;j<m;j++) mat[0][j]=0;
+    }
+    if(col0==0){
+        for(int i=0;i<n;i++) mat[i][0]=0;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
     vector<vector<int>> mat={{1,1,1,1,0},{1,1,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{1,1,1,1,1}};
@@ -80,8 +143,9 @@ int main(){
         cout<<endl;
     }
     cout<<endl;
-    // set(mat);
-    cout<<"Lal bale sir :"<<endl;
-    set1(mat);
+    // set3(mat);
+    OptimalApproach(mat);
+    // cout<<"Lal bale sir :"<<endl;
+    // set1(mat);
     return 0;
 }

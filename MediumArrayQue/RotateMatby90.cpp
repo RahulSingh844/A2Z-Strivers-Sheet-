4c5d5ebcbed30684b6cc74b99d63ace03303e3e6
RahulSingh844 <rahulchd30082005@gmail.com>
2026-09-17 12:54:53 +0530

    Rotate Matrix by 90 degree - Optimal Approach

--- MediumArrayQue/RotateMatby90.cpp
+++ MediumArrayQue/RotateMatby90.cpp
@@ -17,7 +17,24 @@ void rotate(vector<vector<int>>&matrix) {
     }
      
 }
-
+// Optimal optimal 
+void rotateOpt(vector<vector<int>>& matrix) {
+    int n=matrix.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     vector<vector<int>> matrix = {{1 ,2 ,3 ,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
     int n=matrix.size();
@@ -28,7 +45,7 @@ int main(){
         }
         cout<<endl;
     }
-    cout<<endl;
-    rotate(matrix);
+    cout<<"Rotated Matrix: "<<endl;
+    rotateOpt(matrix);
     return 0;
 }
